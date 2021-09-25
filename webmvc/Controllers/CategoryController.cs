af4c31d7e4b82bf6f7e768c41b890801097a6e9e
onweekisday79 <onweekisday79@gmail.com>
2021-09-26 04:24:40 +0800

    新刪修 cdr 功能完成

--- webmvc/Controllers/CategoryController.cs
+++ webmvc/Controllers/CategoryController.cs
@@ -15,5 +15,40 @@ namespace webmvc.Controllers
             List<Category> categories = CategoryDataContext.LoadCategories();
             return View(categories);
         }
+        public ActionResult Insert()
+        {
+            if (Request.Form.Count > 0)
+            {
+                Category _category = new Category();
+                _category.CategoryName = Request.Form["CategoryName"];
+                _category.Description = Request.Form["Description"];
+                CategoryDataContext.InsertCategory(_category);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            Category _category = CategoryDataContext.LoadCategoryByID(id);
+            return View(_category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            Category _category = new Category();
+            _category.CategoryID = Convert.ToInt32(Request.Form["CategoryID"]);
+            _category.CategoryName = Request.Form["CategoryName"];
+            _category.Description = Request.Form["Description"];
+            CategoryDataContext.EditCategory(_category);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id = 0)
+        {
+            CategoryDataContext.DeleteCategory(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- webmvc/Models/CategoryDataContext.cs
+++ webmvc/Models/CategoryDataContext.cs
@@ -10,7 +10,11 @@ namespace webmvc.Models
     {
         static string strConn = System.Configuration.ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
 
-        //讀取所有產品分類資料
+
+        /// <summary>
+        /// 讀取所有產品分類資料
+        /// </summary>
+        /// <returns></returns>
         public static List<Category> LoadCategories()
         {
             List<Category> categories = new List<Category>();
@@ -34,6 +38,91 @@ namespace webmvc.Models
                 }
             }
             return categories;
+        }
+        /// <summary>
+        /// 新增產品分類資料
+        /// </summary>
+        /// <param name="category"></param>
+        public static void InsertCategory(Category category)
+        {
+            string strCmd = "insert Categories(CategoryName,Description)                                values(@CategoryName,@Description)";
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+                    cmd.Parameters.AddWithValue("@Description", category.Description);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="CategoryID"></param>
+        /// <returns></returns>
+        public static Category LoadCategoryByID(int? CategoryID)
+        {
+            Category _category = new Category();
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                string strCmd = "select CategoryID,CategoryName,Description from                       Categories where CategoryID=@CategoryID";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        _category.CategoryID = Convert.ToInt32(dr["CategoryID"]);
+                        _category.CategoryName = dr["CategoryName"].ToString();
+                        _category.Description = dr["Description"].ToString();
+                    }
+                    dr.Close();
+                    conn.Close();
+                }
+            }
+            return _category;
+        }
+        /// <summary>
+        /// 修改頁面
+        /// </summary>
+        /// <param name="category"></param>
+        public static void EditCategory(Category category)
+        {
+            string strCmd = "update Categories set CategoryName=@CategoryName, Description = @Description where CategoryID = @CategoryID";
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+                    cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+                    cmd.Parameters.AddWithValue("@Description", category.Description);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        /// <summary>
+        /// 刪除
+        /// </summary>
+        /// <param name="CategoryID"></param>
+        public static void DeleteCategory(int CategoryID)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                String strCmd = "Delete Categories where CategoryID=@CategoryID";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
         }
 
     }
