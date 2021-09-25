4fca7285b8b88c17c5ea0b2233a4b736cbc495fd
onweekisday79 <onweekisday79@gmail.com>
2021-09-26 03:24:46 +0800

    資料庫查詢增加， 主頁修改。

---
+++ webmvc/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webmvc.Models;
+
+namespace webmvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            List<Category> categories = CategoryDataContext.LoadCategories();
+            return View(categories);
+        }
+    }
+}
---
+++ webmvc/Models/CategoryDataContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace webmvc.Models
+{
+    public class CategoryDataContext
+    {
+        static string strConn = System.Configuration.ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ConnectionString;
+
+        //讀取所有產品分類資料
+        public static List<Category> LoadCategories()
+        {
+            List<Category> categories = new List<Category>();
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                String strCmd = "select CategoryID,CategoryName,Description from Categories";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Category _category = new Category();
+                        _category.CategoryID = Convert.ToInt32(dr["CategoryID"]);
+                        _category.CategoryName = dr["CategoryName"].ToString();
+                        _category.Description = dr["Description"].ToString();
+                        categories.Add(_category);
+                    }
+                    dr.Close();
+                    conn.Close();
+                }
+            }
+            return categories;
+        }
+
+    }
+}
